f63ffa0d1eb4cd46efe6e16daa4bbfd838e13c6c
Fatality <fatihcayir2001@gmail.com>
2021-02-13 22:56:39 +0300

    13 şubat Commit

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -24,10 +24,10 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour==00)
-            {
-                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
-            }
+            //if (DateTime.Now.Hour==00)
+            //{
+            //    return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            //}
             
                 return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
             
@@ -55,6 +55,7 @@ namespace Business.Concrete
             return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDeatils());
         }
 
+        //[LogAspect] --> AOP yani bu kodu logla demek
         public IResult Add(Product product)
         {
 
---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //---> ATTRIBUTE Bir class ile ilgili bilgi verme, imzalama yöntemidir
+    //isimlendirme standartanı göre çağrırken controlleri yazmayız
+    //10.satırda nasıl çağrıcağımız var
+    //bu kodu api/products yazarak çağrabiliriz
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled -- Gevşek bağlılık
+        //IoC Container -- Inersion of Controller   
+        //IoC container bellekte kullkanılacak manager,servisi newleyip tutmak diyebiliriz
+        //Startup da 33 ve 34.satıra bakabilirsin (AddSingleon ile yaptık)
+        
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]   
+        public List<Product> Get()
+        {
+            return _productService.GetAll().Data;
+            
+        }
+
+    }
+}
